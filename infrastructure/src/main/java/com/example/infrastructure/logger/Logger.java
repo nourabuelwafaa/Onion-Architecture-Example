66f09b9710fb1a8e84f854244bb127d122994a94
Nour Ahmed <nour.ahmed@talabat.com>
2024-12-20 20:53:35 +0200

    Cleanup.

--- infrastructure/src/main/java/com/example/infrastructure/logger/Logger.java
+++ infrastructure/src/main/java/com/example/infrastructure/logger/Logger.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Logger implements ILogger {
-    private static final org.slf4j.Logger logger = LoggerFactory.getLogger("[Charity-hub - App]");
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger("[OnionArchi - App]");
 
     @Override
     public void log(String message) {
